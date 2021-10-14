db30259742b825ee535b61c1e40b923db95c2d2d
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2021-10-14 15:00:35 +0900

    feat(Mult.asm)

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,27 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@i
+M=1
+@R2
+M=0
+(LOOP)
+@i // D=i
+D=M
+@R0
+D=M-D // D=R0-i
+@END
+D;JLT // exit if (R0 - i < 0)
+@R1
+D=M // D=Memory[R1]
+@R2
+M=D+M
+@i
+M=M+1
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
+
